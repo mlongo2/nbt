e154a36d1f4621632f2191f9a718e2c165e0a397
mlongo2 <mlongo2@live.com>
2013-08-08 03:01:53 +0000

    test2

---
+++ web.js
@@ -0,0 +1,15 @@
+var express = require('express');
+
+var app = express.createServer(express.logger());
+
+app.get('/', function(request, response) {
+var fs = require('fs');
+var buffer = new Buffer (fs.readFileSync('nbtweb.html'), 'utf-8');
+ 
+    response.send(buffer.toString());
+});
+
+var port = process.env.PORT || 8080;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
